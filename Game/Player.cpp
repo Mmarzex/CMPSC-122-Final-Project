05ff713eeab19dc77b4e633c8c92b10c08143a2c
Max Marze <sync95@gmail.com>
2013-02-23 03:26:51 -0500

    Added Player class

---
+++ Game/Player.cpp
@@ -0,0 +1,9 @@
+//
+//  Player.cpp
+//  Game
+//
+//  Created by Max Marze on 2/23/13.
+//  Copyright (c) 2013 Max Marze. All rights reserved.
+//
+
+#include "Player.h"
---
+++ Game/Player.h
@@ -0,0 +1,14 @@
+//
+//  Player.h
+//  Game
+//
+//  Created by Max Marze on 2/23/13.
+//  Copyright (c) 2013 Max Marze. All rights reserved.
+//
+
+#ifndef __Game__Player__
+#define __Game__Player__
+
+#include <iostream>
+
+#endif /* defined(__Game__Player__) */
